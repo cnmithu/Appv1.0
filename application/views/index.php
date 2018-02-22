bd28e64f706e2d024fc21607dd0a5b1f061640d1
MithuCN <cnmithu@gmail.com>
2018-02-22 16:26:48 +0100

    Add json movie DB and view page

---
+++ application/views/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>" type="text/css" >
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/app.css'); ?>" type="text/css" >
+
+        <title>Movies Collection</title>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Movies Collection</h1>
+                <p class="lead">Global Gaming – Application Challenge v1.0 </p>
+            </div>
+        </div>
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="<?php echo base_url('assets/js/jquery.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+        <script src="<?php echo base_url('assets/js/app.js'); ?>"></script>
+        <script> base_url = '<?php echo base_url() ?>';</script>
+
+    </body>
+</html>
